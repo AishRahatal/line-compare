9a58898e4e1c9971dc74c6be73f1155b4518bb0d
Aishwarya Rahatal <aishwarya.rahatal97@gmail.com>
2021-09-12 16:36:52 +0530

    main class

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace line_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line comparison program!");
+
+            Calculate_length obj = new Calculate_length();
+            obj.get_length();
+            Console.ReadKey();
+        }
+    }
+}
